97ffd3cced5e980be2f5789894aea6ca66c7ab26
Agustin Taibo <taibocruz@gmail.com>
2023-04-19 21:37:30 -0300

    arreglando clase active en los links

--- main.js
+++ main.js
@@ -35,3 +35,31 @@ toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   localStorage.setItem('darkMode', body.classList.contains('dark-mode')); // guarda el estado actual del modo oscuro en localStorage
 });
+
+
+// Obtener los elementos "a" dentro de los "li" en la lista de navegación
+const navLinks = document.querySelectorAll('ul li a');
+
+// Agregar un evento de escucha al desplazamiento de la página
+window.addEventListener('scroll', () => {
+  // Obtener la posición actual del usuario en la página
+  const currentPosition = window.pageYOffset;
+
+  // Comprobar cada sección en la página
+  document.querySelectorAll('section').forEach((section, index) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    const offset = 100;
+
+    // Si el usuario está en una sección, actualizar la clase "active" en la lista de navegación
+    if (currentPosition >= (sectionTop - offset ) && currentPosition < (sectionTop + sectionHeight - offset)) {
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+      });
+
+      // Agregar la clase "active" al elemento "a" correspondiente
+      navLinks[index].classList.add('active');
+    }
+  });
+});
